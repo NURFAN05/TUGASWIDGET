a375a3c49d241667c591f70f18b8ea7e0d082016
NURFAN05 <11950111738@students.uin-suska.ac.id>
2021-05-24 01:20:10 +0700

    TUGAS WIDGET
    tugas pemrograman bergerak

---
+++ WIDGET2/app/src/main/java/com/example/widget/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.widget
+
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val imgBtn = findViewById<ImageButton>(R.id.imgButtonAdd)
+        imgBtn.setOnClickListener {
+            Toast.makeText( this@MainActivity, "Berhasil", Toast.LENGTH_LONG).show()
+        }
+    }
+}
